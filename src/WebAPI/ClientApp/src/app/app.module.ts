5c4482663a887b0c8712aa63d03a3aadf41510d7
ogrodowczyko <ollo9998@gmail.com>
2022-01-01 13:31:26 +0100

    Overall improvements

--- src/WebAPI/ClientApp/src/app/app.module.ts
+++ src/WebAPI/ClientApp/src/app/app.module.ts
@@ -5,16 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './auth/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+import { AccountClient } from './web-api-client';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [LoginComponent, AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [AccountClient],
   bootstrap: [AppComponent],
+  exports: [],
 })
 export class AppModule {}
--- src/WebAPI/ClientApp/src/app/shared/shared.module.ts
+++ src/WebAPI/ClientApp/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './footer/footer.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [NavbarComponent, FooterComponent],
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule],
   exports: [NavbarComponent, FooterComponent],
 })
 export class SharedModule {}
