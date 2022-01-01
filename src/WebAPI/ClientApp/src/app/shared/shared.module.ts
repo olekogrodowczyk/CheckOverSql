6580458e7ccb49fd3eb3659d66f97c0fdf0ed09f
ogrodowczyko <ollo9998@gmail.com>
2022-01-01 18:12:37 +0100

    Implement snackbar service

--- src/WebAPI/ClientApp/src/app/shared/shared.module.ts
+++ src/WebAPI/ClientApp/src/app/shared/shared.module.ts
@@ -5,10 +5,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './footer/footer.component';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { SnackbarService } from './snackbar.service';
 
 @NgModule({
   declarations: [NavbarComponent, FooterComponent],
   imports: [CommonModule, MatToolbarModule, MatButtonModule, RouterModule],
   exports: [NavbarComponent, FooterComponent],
+  providers: [SnackbarService],
 })
 export class SharedModule {}
