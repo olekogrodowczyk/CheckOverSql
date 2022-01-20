f1350d4c61beb62052e1baeacc48d3e82ab4046b
ogrodowczyko <ollo9998@gmail.com>
2022-01-20 09:48:07 +0100

    Add is logged in guard

--- src/WebAPI/ClientApp/src/app/app-routing.module.ts
+++ src/WebAPI/ClientApp/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IsLoggedInGuard } from './auth/is-logged-in.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
   {
+    canActivate: [IsLoggedInGuard],
     path: 'login',
     component: LoginComponent,
   },
   {
+    canActivate: [IsLoggedInGuard],
     path: 'register',
     component: RegisterComponent,
   },
---
+++ src/WebAPI/ClientApp/src/app/auth/is-logged-in.guard.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable, skipWhile, take, tap } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IsLoggedInGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.authService.signedIn$.pipe(
+      skipWhile((value) => value === null),
+      take(1),
+      tap((authenticated: boolean) => {
+        if (authenticated) {
+          this.router.navigateByUrl('/');
+        }
+      })
+    );
+  }
+}
