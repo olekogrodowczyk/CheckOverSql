1204a68adfa5a548a9f3abbc221affe4b3063095
ogrodowczyko <ollo9998@gmail.com>
2022-01-30 20:35:59 +0100

    Create exercise components

---
+++ src/WebAPI/ClientApp/src/app/exercises/create-exercise-form/create-exercise-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-exercise-form',
+  templateUrl: './create-exercise-form.component.html',
+  styleUrls: ['./create-exercise-form.component.css']
+})
+export class CreateExerciseFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/WebAPI/ClientApp/src/app/exercises/exercises.module.ts
+++ src/WebAPI/ClientApp/src/app/exercises/exercises.module.ts
@@ -4,9 +4,18 @@ import { CommonModule } from '@angular/common';
 import { ExercisesRoutingModule } from './exercises-routing.module';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { PublicComponent } from './public/public.component';
+import { CreatedComponent } from './created/created.component';
+import { CreateExerciseFormComponent } from './create-exercise-form/create-exercise-form.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, ExercisesRoutingModule, SharedModule],
+  declarations: [
+    HomeComponent,
+    PublicComponent,
+    CreatedComponent,
+    CreateExerciseFormComponent,
+  ],
+  imports: [CommonModule, MatTabsModule, ExercisesRoutingModule, SharedModule],
 })
 export class ExercisesModule {}
