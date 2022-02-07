2ff081f3a78753d3dbcafe9f08bdadbcd7c362e6
ogrodowczyko <ollo9998@gmail.com>
2022-02-07 22:06:20 +0100

    Remove guard

--- src/WebAPI/ClientApp/src/app/send-query/send-query-routing.module.ts
+++ src/WebAPI/ClientApp/src/app/send-query/send-query-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { IsResultEmptyGuard } from './is-result-empty.guard';
 import { QueryResultComponent } from './query-result/query-result.component';
 
 const routes: Routes = [
   {
     path: 'query-result',
-    canActivate: [IsResultEmptyGuard],
     component: QueryResultComponent,
   },
   { path: '', component: HomeComponent },
@@ -16,6 +14,5 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  providers: [IsResultEmptyGuard],
 })
 export class SendRequestRoutingModule {}
