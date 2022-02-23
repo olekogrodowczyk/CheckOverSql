658ec43cd3f38da0221c214db9e062fd649d6b84
ogrodowczyko <ollo9998@gmail.com>
2022-02-23 16:44:41 +0100

    Navbar improvements

--- src/WebAPI/ClientApp/src/app/shared/shared.module.ts
+++ src/WebAPI/ClientApp/src/app/shared/shared.module.ts
@@ -7,6 +7,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { SnackbarService } from './snackbar.service';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatMenu, MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [NavbarComponent, FooterComponent],
@@ -16,6 +18,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatButtonModule,
     RouterModule,
     MatSnackBarModule,
+    MatMenuModule,
+    MatIconModule,
   ],
   exports: [NavbarComponent, FooterComponent],
   providers: [SnackbarService],
