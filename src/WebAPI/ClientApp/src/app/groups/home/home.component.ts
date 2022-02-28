fff936c078a1fc751f07033559add0a8fa900ced
ogrodowczyko <ollo9998@gmail.com>
2022-02-28 21:12:27 +0100

    Add default route in groups

--- src/WebAPI/ClientApp/src/app/groups/home/home.component.ts
+++ src/WebAPI/ClientApp/src/app/groups/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { SnackbarService } from 'src/app/shared/snackbar.service';
 import {
@@ -15,6 +16,9 @@ import { CreateGroupFormComponent } from '../create-group-form/create-group-form
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router) {
+    this.router.navigateByUrl('groups/(side:groups)');
+  }
   refreshSubject: Subject<boolean> = new Subject<boolean>();
 
   ngOnInit(): void {}
