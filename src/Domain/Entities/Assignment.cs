8cb43c0d3339bf5f8af082827429c9a416231151
ogrodowczyko <ollo9998@gmail.com>
2022-03-05 23:12:32 +0100

    Add constructors into assignment

--- src/Domain/Entities/Assignment.cs
+++ src/Domain/Entities/Assignment.cs
@@ -9,12 +9,23 @@ namespace Domain.Entities
 {
     public class Assignment : AuditableEntity
     {
+        public Assignment()
+        {
+        }
+
+        public Assignment(int userId, int groupId, int groupRoleId)
+        {
+            UserId = userId;
+            GroupId = groupId;
+            GroupRoleId = groupRoleId;
+        }
+
         public int UserId { get; set; }
         public virtual User User { get; set; }
         public int GroupId { get; set; }
-        public virtual Group Group{ get; set; }
+        public virtual Group Group { get; set; }
         public int GroupRoleId { get; set; }
-        public virtual GroupRole GroupRole {  get; set; }
+        public virtual GroupRole GroupRole { get; set; }
 
         public virtual ICollection<Solving> Solvings { get; set; }
     }
