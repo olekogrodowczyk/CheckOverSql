5584fb196b9b58d0b6517d82411ae8bed329dd2b
ogrodowczyko <ollo9998@gmail.com>
2022-03-10 15:16:33 +0100

    Add more check permission tests

--- tests/Application.IntegrationTests/GroupRoles/Queries/CheckPermissionTests.cs
+++ tests/Application.IntegrationTests/GroupRoles/Queries/CheckPermissionTests.cs
@@ -1,4 +1,5 @@
-﻿using Application.GroupRoles.Queries.CheckPermission;
+﻿using Application.Common.Exceptions;
+using Application.GroupRoles.Queries.CheckPermission;
 using Domain.Entities;
 using Domain.Enums;
 using FluentAssertions;
@@ -40,6 +41,42 @@ namespace Application.IntegrationTests.GroupRoles.Queries
 
             //Assert
             result.Should().Be(expectedResult);
+        }
+
+        [Fact]
+        public async Task ForNonExistingGroup_ThrowsValidationException()
+        {
+            //Arrange
+            await ClearNotNecesseryData();
+            int userId = await RunAsDefaultUserAsync();
+            var groupRoles = await SeedPermissionWithGroupRoles();
+            var users = await SeedUsers();
+
+            //Act
+            Func<Task<bool>> func = async () => await SendAsync(new CheckPermissionQuery()
+            { Permission = PermissionEnum.DeletingGroup.ToString(), GroupId = 99 });
+
+            //Assert
+            await func.Should().ThrowAsync<ValidationException>();
+        }
+
+        [Fact]
+        public async Task ForNonExistingPermission_ThrowsValidationException()
+        {
+            //Arrange
+            await ClearNotNecesseryData();
+            int userId = await RunAsDefaultUserAsync();
+            var groupRoles = await SeedPermissionWithGroupRoles();
+            var users = await SeedUsers();
+            var group = await AddAsync(new Group() { Name = "Grupa1", CreatorId = users["user1"] });
+            var assignment = await AddAsync(new Assignment(userId, group.Id, groupRoles["Owner"]));
+
+            //Act
+            Func<Task<bool>> func = async () => await SendAsync(new CheckPermissionQuery()
+            { Permission = "dnjwqoindqwoi", GroupId = 99 });
+
+            //Assert
+            await func.Should().ThrowAsync<ValidationException>();
         }
     }
 }
