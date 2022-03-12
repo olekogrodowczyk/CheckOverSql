2b79d95522a8a5a85a3aec1d5bcb77676469663b
ogrodowczyko <ollo9998@gmail.com>
2022-03-12 18:16:15 +0100

    Handing authorization improvements

--- src/Application/Common/Authorization/PermissionRequirementHandler.cs
+++ src/Application/Common/Authorization/PermissionRequirementHandler.cs
@@ -14,7 +14,7 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Authorization
 {
-    public class PermissionRequirementHandler : AuthorizationHandler<PermissionRequirement, Assignment>
+    public class PermissionRequirementHandler : AuthorizationHandler<PermissionRequirement, Group>
     {
         private readonly IUserContextService _userContextService;
         private readonly IAssignmentRepository _assignmentRepository;
@@ -28,16 +28,20 @@ namespace Infrastructure.Authorization
             _permissionRepository = permissionRepository;
         }
 
-        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement, Assignment assignment)
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement, Group group)
         {
-            if (assignment == null) { throw new ForbidException($"Access denied for permission: {requirement.Permission.GetDisplayName()}", true); }
+            if (context is null || context.User.Claims.Count() == 0) { throw new UnauthorizedAccessException(); }
+            var userId = int.Parse(context.User.FindFirst(x => x.Type == ClaimTypes.NameIdentifier).Value);
+
+            var assignment = await _assignmentRepository.SingleOrDefaultAsync(x => x.UserId == userId && x.GroupId == group.Id);
+            if (assignment is null) { throw new ForbidException($"Access denied for permission: {requirement.Permission.GetDisplayName()}", true); }
+
             if (!await _permissionRepository.AnyAsync(x => x.Title == requirement.Permission.GetDisplayName()))
             {
                 throw new NotFoundException($"Permission {requirement.Permission.GetDisplayName()} cannot be found");
             }
-            var result = await _assignmentRepository.CheckIfAssignmentHasPermission(assignment.Id, requirement.Permission);       
-            if (!result) { throw new ForbidException($"Access denied for permission: {requirement.Permission.GetDisplayName()}", true); }
-            context.Succeed(requirement);
+            var result = await _assignmentRepository.CheckIfAssignmentHasPermission(assignment.Id, requirement.Permission);
+            if (result) { context.Succeed(requirement); }
         }
     }
 }
