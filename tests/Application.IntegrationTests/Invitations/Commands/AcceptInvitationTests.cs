6851d15676f11b93b66d6f53d23097bccd43e331
ogrodowczyko <ollo9998@gmail.com>
2022-03-27 18:22:45 +0200

    Add invitation integration tests

---
+++ tests/Application.IntegrationTests/Invitations/Commands/AcceptInvitationTests.cs
@@ -0,0 +1,55 @@
+﻿using Application.Invitations.Commands.AcceptInvitation;
+using Application.Invitations.Commands.RejectInvitation;
+using Domain.Entities;
+using Domain.Enums;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI;
+using WebAPI.IntegrationTests;
+using Xunit;
+
+namespace Application.IntegrationTests.Invitations.Commands
+{
+    public class AcceptInvitationTests : SharedUtilityClass, IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        public AcceptInvitationTests(CustomWebApplicationFactory<Startup> factory) : base(factory)
+        {
+        }
+
+        [Theory]
+        [InlineData("accept", true)]
+        [InlineData("reject", false)]
+        public async Task ForValidInvitation_ReturnsOk(string queryType, bool assignmentExists)
+        {
+            //Arrange
+            await ClearNotNecesseryData();
+            var userId = await RunAsDefaultUserAsync();
+            var users = await SeedUsers();
+            var groupRoles = await SeedPermissionWithGroupRoles();
+            var group = await AddAsync(new Group { Name = "Grupa1", CreatorId = users["user1"] });
+            var assignment = await AddAsync
+                (new Assignment { GroupId = group.Id, UserId = users["user1"], GroupRoleId = groupRoles["Checker"] });
+            var invitation = await AddAsync(new Invitation
+            {
+                SenderId = users["user1"],
+                ReceiverId = userId,
+                GroupId = group.Id,
+                Status = InvitationStatusEnum.Sent,
+            });
+
+            //Act
+            Func<Task> func = queryType == "accept"
+            ? async () => await SendAsync(new AcceptInvitationCommand { InvitationId = invitation.Id })
+            : async () => await SendAsync(new RejectInvitationCommand { InvitationId = invitation.Id });
+            await func.Invoke();
+
+            //Assert
+            var assignmentsExists = await AnyAsync<Assignment>(x => x.UserId == invitation.ReceiverId);
+            assignmentExists.Should().Be(assignmentExists);
+        }
+    }
+}
---
+++ tests/Application.IntegrationTests/Invitations/Commands/CreateInvitationTests.cs
@@ -0,0 +1,239 @@
+﻿using Application.Common.Exceptions;
+using Application.Invitations.Commands.CreateInvitation;
+using Domain.Entities;
+using FluentAssertions;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI;
+using WebAPI.IntegrationTests;
+using Xunit;
+
+namespace Application.IntegrationTests.Invitations.Commands
+{
+    public class CreateInvitationTests : SharedUtilityClass, IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        public CreateInvitationTests(CustomWebApplicationFactory<Startup> factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task ForValidModel_ReturnsOkWithValidProperties()
+        {
+            //Arrange
+            await ClearNotNecesseryData();
+            var userId = await RunAsDefaultUserAsync();
+            var users = await SeedUsers();
+            var groupRoles = await SeedPermissionWithGroupRoles();
+            var group = await AddAsync(new Group { Name = "Grupa1", CreatorId = userId });
+            var assignment = await AddAsync
+                (new Assignment { GroupId = group.Id, UserId = userId, GroupRoleId = groupRoles["Owner"] });
+
+            var command = new CreateInvitationCommand
+            {
+                ReceiverEmail = "johnsmith@gmail.com",
+                RoleName = "Moderator",
+                GroupId = group.Id,
+            };
+
+            //Act
+            var result = await SendAsync(command);
+
+            //Assert
+            var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+            var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+            var invitation = await context.Invitations.Include(x => x.GroupRole).Include(x => x.Receiver).Include(x => x.Group)
+                .SingleOrDefaultAsync();
+            invitation.Receiver.Email.Should().Be("johnsmith@gmail.com");
+            invitation.Group.Should().NotBeNull();
+            invitation.GroupRole.Name.Should().Be("Moderator");
+        }
+
+        [Fact]
+        public async Task ForSenderIsNotInTheGroup_ReturnsBadRequest()
+        {
+            //Arrange
+            await ClearNotNecesseryData();
+            var userId = await RunAsDefaultUserAsync();
+            var users = await SeedUsers();
+            var groupRoles = await SeedPermissionWithGroupRoles();
+            var group = await AddAsync(new Group { Name = "Grupa1", CreatorId = userId });
+            var assignment = await AddAsync
+                (new Assignment { GroupId = group.Id, UserId = users["user3"], GroupRoleId = groupRoles["Checker"] });
+
+            var command = new CreateInvitationCommand
+            {
+                ReceiverEmail = "johnsmith@gmail.com",
+                RoleName = "Moderator",
+                GroupId = group.Id,
+            };
+
+            //Act
+            Func<Task> func = async () => await SendAsync(command);
+
+            //Assert
+            await func.Should().ThrowAsync<ValidationException>();
+        }
+
+        [Theory]
+        [MemberData(nameof(GetSampleInvalidCreateModels))]
+        public async Task ForInvalidModels_ReturnsBadRequest(CreateInvitationCommand createInvitationDto)
+        {
+            //Arrange
+            await ClearNotNecesseryData();
+            var userId = await RunAsDefaultUserAsync();
+            var users = await SeedUsers();
+            var groupRoles = await SeedPermissionWithGroupRoles();
+            await SeedUsers();
+
+            var group = await AddAsync(new Group { Name = "Grupa1", CreatorId = userId });
+            var assignment = await AddAsync
+                (new Assignment { GroupId = group.Id, UserId = userId, GroupRoleId = groupRoles["Checker"] });
+
+            //Act
+            Func<Task> func = async () => await SendAsync(createInvitationDto);
+
+            //Assert
+            await func.Should().ThrowAsync<ValidationException>();
+        }
+
+        [Fact]
+        public async Task ForTwoSameInvitations_ReturnsBadRequest()
+        {
+            //Arrange
+            await ClearNotNecesseryData();
+            var userId = await RunAsDefaultUserAsync();
+            var users = await SeedUsers();
+            var groupRoles = await SeedPermissionWithGroupRoles();
+            var group = await AddAsync(new Group { Name = "Grupa1", CreatorId = userId });
+            var assignment = await AddAsync
+                (new Assignment { GroupId = group.Id, UserId = userId, GroupRoleId = groupRoles["Moderator"] });
+
+            var command = new CreateInvitationCommand
+            {
+                ReceiverEmail = "johnsmith@gmail.com",
+                RoleName = "Moderator",
+                GroupId = group.Id,
+            };
+
+            //Act
+            var result = await SendAsync(command);
+            Func<Task> func = async () => await SendAsync(command);
+
+            //Assert
+            result.Should().BeGreaterThan(0);
+            await func.Should().ThrowAsync<ValidationException>();
+        }
+
+        [Fact]
+        public async Task ForUserAlreadyInGroup_ReturnsBadRequest()
+        {
+            //Arrange
+            await ClearNotNecesseryData();
+            var userId = await RunAsDefaultUserAsync();
+            var users = await SeedUsers();
+            var groupRoles = await SeedPermissionWithGroupRoles();
+            var group = await AddAsync(new Group { Name = "Grupa1", CreatorId = userId });
+            var assignment = await AddAsync
+                (new Assignment { GroupId = group.Id, UserId = users["user1"], GroupRoleId = groupRoles["Checker"] });
+
+            var command = new CreateInvitationCommand
+            {
+                ReceiverEmail = "testinvitation@gmail.com",
+                RoleName = "Moderator"
+            };
+
+            //Act
+            Func<Task> func = async () => await SendAsync(command);
+
+            //Assert
+            await func.Should().ThrowAsync<ValidationException>();
+        }
+
+        [Fact]
+        public async Task ForInvalidGroupRoleInAssignment_ReturnsForbidden()
+        {
+            //Arrange
+            await ClearNotNecesseryData();
+            var userId = await RunAsDefaultUserAsync();
+            var users = await SeedUsers();
+            var groupRoles = await SeedPermissionWithGroupRoles();
+
+            var group = await AddAsync(new Group { Name = "Grupa1", CreatorId = userId });
+            var assignment = await AddAsync
+                (new Assignment { GroupId = group.Id, UserId = userId, GroupRoleId = groupRoles["Checker"] });
+
+            var command = new CreateInvitationCommand
+            {
+                ReceiverEmail = "johnsmith@gmail.com",
+                RoleName = "Moderator",
+                GroupId = group.Id
+            };
+
+            //Act
+            Func<Task> func = async () => await SendAsync(command);
+
+            //Assert
+            await func.Should().ThrowAsync<ForbidException>();
+        }
+
+        [Fact]
+        public async Task ForSendingInvitationForYourself_ReturnsBadRequest()
+        {
+            //Arrange
+            await ClearNotNecesseryData();
+            var userId = await RunAsDefaultUserAsync();
+            var users = await SeedUsers();
+            var groupRoles = await SeedPermissionWithGroupRoles();
+            var group = await AddAsync(new Group { Name = "Grupa1", CreatorId = userId });
+            var assignment = await AddAsync
+                (new Assignment { GroupId = group.Id, UserId = userId, GroupRoleId = groupRoles["Moderator"] });
+
+            var command = new CreateInvitationCommand
+            {
+                ReceiverEmail = "user@gmail.com",
+                RoleName = "Moderator",
+                GroupId = group.Id,
+            };
+
+            //Act
+            Func<Task> func = async () => await SendAsync(command);
+
+            //Assert
+            await func.Should().ThrowAsync<ValidationException>();
+        }
+
+        public static IEnumerable<object[]> GetSampleInvalidCreateModels()
+        {
+            var list = new List<CreateInvitationCommand>()
+            {
+                new CreateInvitationCommand
+                {
+                    ReceiverEmail = "dsnaudnas@dsnmaiod.ssa",
+                    RoleName = "Moderator",
+                    GroupId = 1
+                },
+                new CreateInvitationCommand
+                {
+                    ReceiverEmail = "testinvitation@gmail.com",
+                    RoleName = "ndsjas",
+                    GroupId = 1
+                },
+                new CreateInvitationCommand
+                {
+                    ReceiverEmail = "dsnaudnas@dsnmaiod.ssa",
+                    RoleName = "ndsjas",
+                    GroupId = 1
+                }
+            };
+            return list.Select(x => new object[] { x });
+        }
+    }
+}
