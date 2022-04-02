3cb529cbd469874ee983a44eb3563941de338e47
ogrodowczyko <ollo9998@gmail.com>
2022-04-02 20:50:55 +0200

    Implement changing role in a group

---
+++ src/Application/GroupRoles/Commands/ChangeRoleCommand.cs
@@ -0,0 +1,61 @@
+﻿using Application.Authorization;
+using Application.Common.Exceptions;
+using Application.Interfaces;
+using Domain.Enums;
+using Domain.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.GroupRoles.Commands
+{
+    public class ChangeRoleCommand : IRequest<int>
+    {
+        public string RoleName { get; set; }
+        public int UserId { get; set; }
+        public int GroupId { get; set; }
+    }
+
+    public class ChangeGroupRoleCommandHandler : IRequestHandler<ChangeRoleCommand, int>
+    {
+        private readonly IGroupRoleRepository _groupRoleRepository;
+        private readonly IUserContextService _userContextService;
+        private readonly IAssignmentRepository _assignmentRepository;
+        private readonly IAuthorizationService _authorizationService;
+        private readonly IGroupRepository _groupRepository;
+
+        public ChangeGroupRoleCommandHandler(IGroupRoleRepository groupRoleRepository, IUserContextService userContextService
+            , IAssignmentRepository assignmentRepository, IAuthorizationService authorizationService, IGroupRepository groupRepository)
+        {
+            _groupRoleRepository = groupRoleRepository;
+            _userContextService = userContextService;
+            _assignmentRepository = assignmentRepository;
+            _authorizationService = authorizationService;
+            _groupRepository = groupRepository;
+        }
+
+        public async Task<int> Handle(ChangeRoleCommand request, CancellationToken cancellationToken)
+        {
+            var assignment = await _assignmentRepository
+                .SingleOrDefaultAsync(x => x.UserId == request.UserId && x.GroupId == request.GroupId);
+            if (assignment is null) { throw new NotFoundException(nameof(assignment), request.UserId); }
+
+            var groupRole = await _groupRoleRepository.FirstOrDefaultAsync(x => x.Name == request.RoleName);
+            if (groupRole is null) { throw new NotFoundException(nameof(groupRole), request.RoleName); }
+
+            var group = await _groupRepository.GetByIdAsync(request.GroupId);
+            var authorizationResult = await _authorizationService.AuthorizeAsync(_userContextService.UserClaimPrincipal
+                , group, new PermissionRequirement(PermissionEnum.ChangingRoles));
+            if (!authorizationResult.Succeeded) { throw new ForbidException(PermissionEnum.ChangingRoles); }
+
+            assignment.GroupRoleId = groupRole.Id;
+            await _assignmentRepository.UpdateAsync(assignment);
+            return assignment.Id;
+        }
+    }
+}
---
+++ src/Application/GroupRoles/Commands/ChangeRoleCommandValidator.cs
@@ -0,0 +1,45 @@
+﻿using Application.Interfaces;
+using Domain.Interfaces;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.GroupRoles.Commands
+{
+    public class ChangeRoleCommandValidator : AbstractValidator<ChangeRoleCommand>
+    {
+        private readonly IGroupRoleRepository _groupRoleRepository;
+        private readonly IUserContextService _userContextService;
+
+        public ChangeRoleCommandValidator(IGroupRoleRepository groupRoleRepository, IUserContextService userContextService)
+        {
+            _groupRoleRepository = groupRoleRepository;
+            _userContextService = userContextService;
+
+            RuleFor(x => x.UserId)
+                .NotEmpty().WithMessage("Specified user cannot be empty")
+                .MustAsync(CheckForCreator).WithMessage("Creators cannot change their roles");
+
+            RuleFor(x => x.GroupId)
+                .NotEmpty().WithMessage("Specified group cannot be empty");
+
+            RuleFor(x => x.RoleName)
+                .NotEmpty().WithMessage("Specified role name cannot be empty");
+        }
+
+        public async Task<bool> CheckForCreator(int userId, CancellationToken cancellationToken)
+        {
+            int? loggedUserId = _userContextService.GetUserId;
+            if (loggedUserId is null) { throw new UnauthorizedAccessException(); }
+
+            if ((int)loggedUserId == userId) { return false; }
+            return true;
+        }
+
+
+    }
+}
