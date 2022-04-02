05044444f7657611419acb17c941d8fa9eec172a
ogrodowczyko <ollo9998@gmail.com>
2022-04-02 22:02:45 +0200

    Implement leaving group

---
+++ src/Application/Groups/Commands/LeaveGroup/LeaveGroupCommand.cs
@@ -0,0 +1,53 @@
+﻿using Application.Common.Exceptions;
+using Application.Interfaces;
+using Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Groups.Commands.LeaveGroup
+{
+    public class LeaveGroupCommand : IRequest<Unit>
+    {
+        public int GroupId { get; set; }
+    }
+
+    public class LeaveGroupCommandHandler : IRequestHandler<LeaveGroupCommand, Unit>
+    {
+        private readonly IUserContextService _userContextService;
+        private readonly IAssignmentRepository _assignmentRepository;
+        private readonly IGroupRepository _groupRepository;
+
+        public LeaveGroupCommandHandler(IUserContextService userContextService, IAssignmentRepository assignmentRepository
+            , IGroupRepository groupRepository)
+        {
+            _userContextService = userContextService;
+            _assignmentRepository = assignmentRepository;
+            _groupRepository = groupRepository;
+        }
+
+        public async Task<Unit> Handle(LeaveGroupCommand request, CancellationToken cancellationToken)
+        {
+            int? loggedUserId = _userContextService.GetUserId;
+            if (loggedUserId is null) { throw new UnauthorizedAccessException(); }
+
+            var group = await _groupRepository.GetByIdAsync(request.GroupId);
+            if (group is null) { throw new NotFoundException(nameof(group), request.GroupId); }
+
+            var assignment = await _assignmentRepository
+                .SingleOrDefaultAsync(x => x.UserId == (int)loggedUserId && x.GroupId == request.GroupId);
+            if (assignment is null)
+            { throw new NotFoundException($"Assignment cannot be found with user: {(int)loggedUserId} and group: {request.GroupId}"); }
+
+            if (group.CreatorId == (int)loggedUserId)
+            { throw new ForbidException("You cannot leave your group while being a creator", true); }
+
+            await _assignmentRepository.DeleteAsync(assignment.Id);
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Application/Groups/Commands/LeaveGroup/LeaveGroupCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Exceptions;
+using Application.Interfaces;
+using Domain.Interfaces;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Groups.Commands.LeaveGroup
+{
+    public class LeaveGroupCommandValidator : AbstractValidator<LeaveGroupCommand>
+    {
+        private readonly IUserContextService _userContextService;
+        private readonly IGroupRepository _groupRepository;
+
+        public LeaveGroupCommandValidator(IUserContextService userContextService, IGroupRepository groupRepository)
+        {
+            _userContextService = userContextService;
+            _groupRepository = groupRepository;
+
+            RuleFor(x => x.GroupId)
+                .NotEmpty().WithMessage("Specified group cannot be empty");
+        }
+
+
+
+    }
+}
